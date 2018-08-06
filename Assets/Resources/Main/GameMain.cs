75c4f00d1009762d69f0c47294b9ea5177a770a1
abotkugyu <abot.kugyu@gmail.com>
2018-08-07 03:11:47 +0900

    add model

--- Assets/Resources/Main/GameMain.cs
+++ Assets/Resources/Main/GameMain.cs
@@ -6,14 +6,29 @@ public class GameMain : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameMap map = GetComponent<GameMap>();
-		map.create ();
+        
 
 	}
-	
+    public int phase = 2;
+    public bool is_create_map = false;
 	// Update is called once per frame
 	void Update () {
-		var conf = ConfigProvider.Api;
+        
+		//var conf = ConfigProvider.Api;
+
+        //rest_map=1,dungen_map=1
+        if (phase == 1) {
+            
+        } else if (phase == 2) {
+            
+
+        }
+        if (is_create_map == false)
+        {
+            GameMap map = GetComponent<GameMap>();
+            map.create();
+            is_create_map = !is_create_map;
+        }
 	}
 		
 }
---
+++ Assets/Resources/Main/Model/GameStatus.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameStatus
+{
+    //座標
+    public int turn;
+    public int map_type;
+}
