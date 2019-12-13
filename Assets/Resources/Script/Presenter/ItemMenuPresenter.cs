3624cf58f4ed4021eed4d21194d3093ea07e19de
ryoji-sato <ryoji.sato@dena.com>
2019-12-13 19:16:14 +0900

    アイテムカーソル実装

--- Assets/Resources/Script/Presenter/ItemMenuPresenter.cs
+++ Assets/Resources/Script/Presenter/ItemMenuPresenter.cs
@@ -6,7 +6,7 @@ public class ItemMenuPresenter : MonoBehaviour {
 	
 	public ItemMenuView itemMenuView;
 	public List<ItemModel> itemsModels;
-	private int selectedId = 0;
+	private int selectedId = 1;
 	
 	public void Initialize(List<ItemModel> models)
 	{
@@ -42,9 +42,19 @@ public class ItemMenuPresenter : MonoBehaviour {
 		{
 			if (Input.GetKeyDown(KeyCode.DownArrow))
 			{
+				if (itemsModels.Count == selectedId)
+				{
+					selectedId = 1;
+				}
+				itemMenuView.SetCursor(++selectedId);
 			}
 			else if (Input.GetKeyDown(KeyCode.UpArrow))
 			{
+				if (itemsModels.Count == 1)
+				{
+					selectedId = itemsModels.Count;
+				}
+				itemMenuView.SetCursor(--selectedId);
 			}
 		}
 	}
--- Assets/Resources/Script/View/ItemMenuView.cs
+++ Assets/Resources/Script/View/ItemMenuView.cs
@@ -7,9 +7,10 @@ public class ItemMenuView : MonoBehaviour {
 	
 	[SerializeField]
 	public GameObject itemWindow;
-	
-	private int item_cell_width = 100;
-	private int item_cell_height = -20;
+		
+	public GameObject cursor;
+	private int itemCellWidth = 100;
+	private int itemCellHeight = -20;
 	
 	[SerializeField]
 	public List<GameObject> itemCellViewList;
@@ -37,16 +38,36 @@ public class ItemMenuView : MonoBehaviour {
 	{
 		itemCellViewList.ForEach(Destroy);
 		itemCellViewList.Clear();
-		GameObject res = Resources.Load("Object/ItemWindow/ItemCell") as GameObject;
+		GameObject res1 = Resources.Load("Object/ItemWindow/ItemCell") as GameObject;
 		for (int i = 0; i < itemModels.Count; i++)
 		{
-			GameObject itemCell = Instantiate (res, itemWindow.transform, true);
-			itemCell.transform.localPosition = new Vector2(250 / -10 ,(Screen.height - 150) / 2 + item_cell_height * i  );
+			GameObject itemCell = Instantiate (res1, itemWindow.transform, true);
+			itemCell.transform.localPosition = GetItemCellPosition(i);
 			itemCellViewList.Add(itemCell);
 			
 			ItemCellView itemCellView = itemCell.GetComponent<ItemCellView>();
 			itemCellView.Name.text = itemModels[i].name;
-		}		
+		}
+		
+		// cursor
+		GameObject res2 = Resources.Load("Object/ItemWindow/Cursol") as GameObject;		
+		cursor = Instantiate (res2, itemWindow.transform, true);
+		cursor.transform.localPosition = GetCursorPosition(0);	
+	}
+
+	public void SetCursor(int seq)
+	{
+		// 計算上-1しておく
+		cursor.transform.localPosition = GetCursorPosition(seq-1);
+	}
+	
+	private Vector2 GetCursorPosition(int seq)
+	{	
+		return new Vector2(50 ,(Screen.height - 150) / 2 + itemCellHeight * seq  );
+	}
+	private Vector2 GetItemCellPosition(int seq)
+	{	
+		return new Vector2(250 / -10 ,(Screen.height - 150) / 2 + itemCellHeight * seq  );
 	}
 }
 
